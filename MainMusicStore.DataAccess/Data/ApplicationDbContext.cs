579e805497702d35067bb87305e529fd642648cf
Onurcan Şimşek <osimsek@biletall.com>
2024-09-22 19:55:40 +0300

    Add project files.

---
+++ MainMusicStore.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MainMusicStore.Models.DbModels;
+
+namespace MainMusicStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
